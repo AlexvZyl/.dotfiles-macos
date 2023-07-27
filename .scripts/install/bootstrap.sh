825b031923d94cc59e897c3378e563d4d9699daa
AlexvZyl <alexandervanzyl@protonmail.com>
2023-07-27 11:55:42 +0200

    Add some scripts

---
+++ .scripts/install/bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Clone.
+brew install git
+mkdir ~/.dotfiles
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME clone --bare https://github.com/AlexvZyl/.dotfiles-macos ~/.dotfiles/
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
+
+# Setup git.
+gh auth login
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
+# Get modules.
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --recursive --remote
+
+# Run main install script.
+~/.scripts/install/install.sh
